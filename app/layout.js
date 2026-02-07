445c8a321f0925380a97cdd27f439066d236271c
roxen316 <helpinghand0555@gmail.com>
2026-02-07 12:41:36 +0530

    layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,11 +1,18 @@
 export const metadata = {
-  title: "Rewari Directory"
+  title: "Rewari Premium Directory"
 }
 
 export default function RootLayout({ children }) {
   return (
     <html>
-      <body>{children}</body>
+      <body style={{
+        margin:0,
+        background:"#050505",
+        color:"#eaeaea",
+        fontFamily:"Inter, sans-serif"
+      }}>
+        {children}
+      </body>
     </html>
   )
 }
