74be9608a656e02551e9f0f10f96711e3caa9101
roxen316 <helpinghand0555@gmail.com>
2026-02-07 12:26:52 +0530

    Create next.config.js file

---
+++ next.config.js
@@ -0,0 +1,12 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    unoptimized: true,
+  },
+  env: {
+    NEXTAUTH_URL: process.env.NEXTAUTH_URL,
+  },
+};
+
+module.exports = nextConfig;
